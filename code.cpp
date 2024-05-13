5d398a11e0febd1b5ea0199e2ca8853c57dbc798
rayankhan-code <55269048+rayankhan-code@users.noreply.github.com>
2024-05-13 23:08:23 +0500

    Added another class

--- code.cpp
+++ code.cpp
@@ -198,6 +198,219 @@ class snake_movements: public data {//inheritance from class data
 			cout<<"Snake Head: ("<<get_s_x()<<", "<<get_s_y()<<")"<<endl;//printing snake's dimension to make it easier to find issue in future if needed
   		 	cout<<"Fruit: ("<<get_f_x()<<", "<<get_f_y()<<")"<<endl;//same as above
 		}
+void snake_movement(int rigid_boundry)//control snake's body and also control head movement
+		{
+			static bool isPaused = false;
+			if(get_game_pause()==' ')
+			{
+				//left empty to pause the game till next input
+			}
+			else
+			{
+				for (int i=get_size()-1;i> 0;--i)
+   				{		//every body box of snake should follow box next to it(forexample if position of head is 2,2 then in next movement position of body should be 2,2)
+   		     		set_body_x(get_body_x(i-1),i);
+   		     		set_body_y(get_body_y(i-1),i);
+				//	body_x[i] = body_x[i - 1];     left to be get help in future
+   		 		//	body_y[i] = body_y[i - 1];
+   		 		}
+			}
+	
+	
+			switch(get_move())//setting snake movement 
+			{
+				case 'w':
+					set_s_y(get_s_y()-1);
+					break;
+				case 's':
+					set_s_y(get_s_y()+1);
+					//s_y++;
+					break;
+				case 'a':
+					set_s_x(get_s_x()-1);
+					//s_x--;
+					break;
+				case 'd':
+					set_s_x(get_s_x()+1);
+					//s_x++;
+					break;
+				case ' '://to pause game
+					isPaused = !isPaused;
+					set_move(get_move1()) ;
+					break;
+				default:
+					break;
+			}
+			if(rigid_boundry==2)//to check which type of boundry user had selected
+			{		//it should only teleport snake if bounder is teleportable
+				switch(get_s_x())//if snakes dimension on x-axis goes out of the box
+				{
+					case 15:
+						set_s_x(2);
+						break;
+					case 1:
+						set_s_x(14);
+						break;
+				}
+				switch(get_s_y())//if snakes dimension on y-axis goes out of the box
+				{
+					case 15:
+						set_s_y(2);
+						break;
+					case 1:
+						set_s_y(14);
+						break;
+				}
+			}
+			set_body_x(get_s_x(),0);
+			
+			set_body_x(get_s_x(),0);
+			set_body_y(get_s_y(),0);
+			//body_x[0] = s_x;
+  			//body_y[0] = s_y;
+		}
+		int srand_check()//restrict new fruit to render on snake's body
+		{
+			for(int i=0;i<get_size();i++)
+			{
+				if(get_f_x()==get_body_x(i)&&get_f_y()==get_body_y(i)||get_f_x()==get_s_x()&&get_f_y()==get_s_y())//checking dimension of snake's head and body along with fruit dimension
+				{
+					return 1;
+				}
+			}
+			return 0;
+		}
+		void consol_set_handler()//handle console setter
+		{
+			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Get handle to console
+  			CONSOLE_CURSOR_INFO cursorInfo; 
+  			GetConsoleCursorInfo(hConsole, &cursorInfo);
+  			cursorInfo.bVisible = false; // Hide cursor
+    		SetConsoleCursorInfo(hConsole, &cursorInfo);
+  			COORD position = {0, 0}; // Set the cursor position to (0, 0)
+  			SetConsoleCursorPosition(hConsole, position);
+		}
+		void input()//contain conio's function _kbhit and _getch
+		{
+			if(_kbhit())//if keyboard hit(pre defined function in conio)
+			{
+				set_move1(_getch());
+				//move1=_getch();//save input to char(pre defined function in conio)
+				set_game_pause(get_move1());
+				//game_pause=move1;
+			}
+			if(get_move()=='w'&&get_move1()=='s'||get_move()=='s'&&get_move1()=='w'||get_move()=='a'&&get_move1()=='d'||get_move()=='d'&&get_move1()=='a')
+			{
+				system("cls");//clear whole console
+				cout<<"\n\n\n\t\tYou can not do in opposite direction!!!";
+				Sleep(2000);//sleep fun is a pre defined function use to stop next output for given milliseconds
+			
+				system("cls");//clear whole console
+				cout<<"\n\n\n\t\t\tResuming Game in: \n\n\t\t\t\t3";
+				Sleep(1000);//sleep fun is a pre defined function use to stop next output for given milliseconds
+			
+				system("cls");//clear whole console
+				cout<<"\n\n\n\t\t\tResuming Game in: \n\n\t\t\t\t2";
+				Sleep(1000);//sleep fun is a pre defined function use to stop next output for given milliseconds
+			
+				system("cls");//clear whole console
+				cout<<"\n\n\n\t\t\tResuming Game in: \n\n\t\t\t\t1";
+				Sleep(1000);//sleep fun is a pre defined function use to stop next output for given milliseconds
+			
+				system("cls");//clear whole console
+				set_move1(get_move());
+				//move1=move;
+			}
+			else
+			{
+				set_move(get_move1());
+				//move=move1;
+			}
+		}
+		int set_level()//take input of level
+		{
+			int level;
+			cout<<"\n\t\t\tPlease Select Level:\n\t\t\t1.Press '1' for v.easy\n\t\t\t2.Press '2' for easy\n\t\t\t3.Press '3' for medium\n\t\t\t4.Press '4' for dificult\n\t\t\t";
+			cin>>level;
+			if(level==1)
+			{
+				level=200;
+			}
+			else if(level==2)
+			{
+				level=150;
+			}
+			else if(level==3)
+			{
+				level=100;
+			}
+			else if(level==4)
+			{
+				level=70;
+			}
+			else
+			{
+				cout<<"\n\n\n\t\t\tInvalid Input!!!\n\n";
+				Sleep(1000);//sleep fun is a pre defined function use to stop next output for given milliseconds
+				set_level();
+			}
+	
+			system("cls");//clear whole console
+			cout<<"\n\n\n\t\t\tStarting Game in: \n\n\t\t\t\t3";
+			cout<<"\n\n\t\t\tUse WASD to move snake\n\t\t\tPress Space Key to Pause Game"<<endl;
+			Sleep(1000);//sleep fun is a pre defined function use to stop next output for given milliseconds
+			
+			system("cls");//clear whole console
+			cout<<"\n\n\n\t\t\tStarting Game in: \n\n\t\t\t\t2";
+			cout<<"\n\n\t\t\tUse WASD to move snake\n\t\t\tPress Space Key to Pause Game"<<endl;
+			Sleep(1000);//sleep fun is a pre defined function use to stop next output for given milliseconds
+			
+			system("cls");//clear whole console
+			cout<<"\n\n\n\t\t\tStarting Game in: \n\n\t\t\t\t1";
+			cout<<"\n\n\t\t\tUse WASD to move snake\n\t\t\tPress Space Key to Pause Game"<<endl;
+			Sleep(1000);//sleep fun is a pre defined function use to stop next output for given milliseconds
+			system("cls");//clear whole console
+	
+			return level;
+		}
+		int game_over(int rigid_boundry)//control game over conditions
+		{
+			for(int i=1;i<get_size();i++)
+			{
+				if(get_body_x(i)==get_s_x()&&get_body_y(i)==get_s_y())//if snake's head conside with its body
+				{
+					system("color 4");
+					Sleep(2000);
+					system("color F");
+					system("cls");//to clear console
+				cout<<"\n\n\n\t\t\033[1;31mGAME OVER!\033[0m\n";
+					return 2;
+				}
+			}
+			if(rigid_boundry==1)//checking type of box walls selected by user
+			{
+				if(get_s_x()==1||get_s_x()==15||get_s_y()==1||get_s_y()==15)//if snake hit boundry of box
+				{
+					system("color 4");
+					Sleep(2000);
+					system("color F");
+					system("cls");//to clear console
+					cout<<"\n\n\n\t\t\033[1;31mGAME OVER!\033[0m\n";
+					return 2;
+				}
+			}
+			return 1;
+		}
+		int rigid_boundry_fun()//input of walls of container/box
+		{
+			int a;
+			cout<<"\t\t\tWhat type of boundry you want: "<<endl;
+			cout<<"\t\t\t1.Press '1' for Fix Boundries\n\t\t\t2.Press'2' for Teleportable Boundries\n\t\t\t";
+			cin>>a;
+			return a;
+		}
+};
+
 
 int main()
 {
